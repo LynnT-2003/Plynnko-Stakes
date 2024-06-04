22c399113f8597a6f482928793ef84c21425e42e
LynnT-2003 <u6411271@au.edu>
2024-06-04 12:26:22 +0700

    Completed initial gameplay with balance and betting amount.

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -4,6 +4,7 @@ import { BallManager } from "../GameUtil/classes/BallManager";
 import { outcomes } from './outcomes';
 import { multipliers } from "./outcomes";
 import Button from '@mui/material/Button';
+import { TextField } from "@mui/material";
 
 const TOTAL_DROPS = 16;
 
@@ -31,7 +32,8 @@ function calculateGameResult() {
 
 export default function Game() {
   const [ballManager, setBallManager] = useState<BallManager | null>(null);
-  const [balance, setBalance] = useState(0); // Initial balance
+  const [balance, setBalance] = useState(0);
+  const [bettingAmount, setBettingAmount] = useState(0);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
@@ -39,8 +41,8 @@ export default function Game() {
       const onFinish = (index: number, startX?: number) => {
         const sink = ballManager?.sinks[index];
         const multiplier = sink?.multiplier || 1;
-        setBalance(prevBalance => {
-          const newBalance = prevBalance * multiplier;
+        setBalance(bettingAmount => {
+          const newBalance = bettingAmount * multiplier;
           console.log(`Balance: ${newBalance}`);
           return newBalance;
         });
@@ -52,14 +54,34 @@ export default function Game() {
   }, [canvasRef]);
 
   return (
-    <div className="flex justify-center items-center">
-      <canvas ref={canvasRef} width="800" height="800"></canvas>
-      <Button variant="contained" color="success" onClick={() => {
-        const result = calculateGameResult();
-        if (ballManager) {
-          ballManager.addBall(result.point);
-        }
-      }}>Add ball</Button>
+    <div>
+      <div className="flex justify-center items-center">
+        <canvas ref={canvasRef} width="800" height="800"></canvas>
+        <div className="flex-col">
+          <Button variant="contained" color="success" onClick={() => {
+            const result = calculateGameResult();
+            if (ballManager) {
+              ballManager.addBall(result.point);
+            }
+            setBalance(balance - bettingAmount)
+          }}>Add ball</Button>
+          <TextField
+            className="w-full"
+            onChange={(e) => setBalance(Number(e.target.value))}
+            value={balance}
+            type="number"
+            label="Balance"
+          />
+          <TextField
+            className="w-full"
+            onChange={(e) => setBettingAmount(Number(e.target.value))}
+            value={bettingAmount}
+            type="number"
+            label="Betting Amount"
+          />
+        </div>
+      </div>
     </div>
+    
   );
 }
