1225588dab60ae85f822796615fbee40f4531a3a
LynnT-2003 <u6411271@au.edu>
2024-06-05 14:31:12 +0700

    Some UI updates

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -3,8 +3,9 @@ import { useEffect, useRef, useState } from "react";
 import { BallManager } from "../GameUtil/classes/BallManager";
 import { outcomes } from './outcomes';
 import { multipliers } from "./outcomes";
-import Button from '@mui/material/Button';
 import { TextField } from "@mui/material";
+import {Button} from "@mui/material";
+import { Input } from "../components/ui/input";
 
 const TOTAL_DROPS = 16;
 
@@ -63,7 +64,7 @@ export default function Game() {
 
   return (
     <div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center" style={{height:"90vh"}}>
         <canvas ref={canvasRef} width="800" height="800"></canvas>
         <div className="flex-col space-y-8">
 
@@ -81,28 +82,31 @@ export default function Game() {
             }}
           />
           
-          <TextField
-            className="w-full text-white"
-            onChange={(e) => setBettingAmount(Number(e.target.value))}
-            value={bettingAmount}
-            type="number"
-            label="Betting Amount (Baht)"
-            InputProps={{
-              className: "text-white"
-            }}
-            InputLabelProps={{
-              className: "text-white"
-            }}
-          />
+          <span>
+            <h1 className="pl-3">Balance: {balance}</h1>
+          </span>
+          
 
-          <Button variant="contained" color="success" onClick={() => {
-            const result = calculateGameResult();
-            if (ballManager) {
-              ballManager.addBall(result.point);
-            }
-            setBalance(balance - bettingAmount)
-            console.log("Remaining amount after betting: ", balance)
-          }}>Add ball</Button>
+          <div className="flex flex-col w-full space-x-2">
+            <h1 className="pl-3 pb-2">Betting Amount:</h1>
+            <Input type="number" placeholder="(THB)" 
+            onChange={(e) => setBettingAmount(Number(e.target.value))}/>
+          </div>
+
+          <div className="flex flex-col space-y-2 pl-2">
+            <Button variant="contained" color="success" onClick={() => {
+              const result = calculateGameResult();
+              if (ballManager) {
+                ballManager.addBall(result.point);
+              }
+              setBalance(balance - bettingAmount)
+              console.log("Remaining amount after betting: ", balance)
+            }}>Add ball</Button>
+
+            <Button variant="contained" color="primary">
+              Top Up Balance
+            </Button>
+          </div>
 
         </div>
       </div>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
   return (
-    <nav className="bg-white z-50 border-gray-200 dark:bg-[#262522] borbder-b shadow-lg">
+    <nav className="bg-white z-50 border-gray-200 bg-slate-800 borbder-b shadow-lg">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div
           onClick={() => router.push("/")}
@@ -25,7 +25,7 @@ export const Navbar = () => {
         </div>
         <Button
           data-collapse-toggle="navbar-default"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 bg-transparent"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 dark:text-gray-400 dark:hover:bg-gray-200 dark:focus:ring-gray-600 bg-transparent"
           onClick={() => {
             setIsMenuOpen(!isMenuOpen);
           }}
@@ -36,12 +36,12 @@ export const Navbar = () => {
         <div
           className={`w-full lg:hidden flex flex-col md:w-auto items-center ${
             isMenuOpen ? "" : "hidden"
-          }`}
+          }`} 
           id="navbar-default"
         >
           <Button
             className="bg-transparent mx-4 hover:bg-black w-[50%]"
-            onClick={() => router.push("/simulation")}
+            onClick={() => router.push("/Simulation")}
           >
             Simulation
           </Button>
@@ -55,7 +55,7 @@ export const Navbar = () => {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <Button
             className="bg-transparent mx-4 hover:bg-black"
-            onClick={() => router.push("/simulation")}
+            onClick={() => router.push("/Simulation")}
           >
             Simulation
           </Button>
