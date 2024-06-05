36f73d6e92a4bd01382a93b6390206a20d5a4159
LynnT-2003 <u6411271@au.edu>
2024-06-05 14:34:15 +0700

    Update Navbar bg to black.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
   return (
-    <nav className="bg-white z-50 border-gray-200 bg-slate-800 borbder-b shadow-lg">
+    <nav className="bg-white z-50 border-gray-200 bg-black borbder-b shadow-lg">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div
           onClick={() => router.push("/")}
