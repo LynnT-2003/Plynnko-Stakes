5135cda605807cd3c4ddaa3905e20a9ce2ba9187
LynnT-2003 <u6411271@au.edu>
2024-06-07 10:10:17 +0700

    Added Shadcn Ui components and Completed topup functionality with dialog.

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -4,8 +4,21 @@ import { BallManager } from "../GameUtil/classes/BallManager";
 import { outcomes } from './outcomes';
 import { multipliers } from "./outcomes";
 import { TextField } from "@mui/material";
-import {Button} from "@mui/material";
+import { Button as ButtonMui } from "@mui/material";
+import { Button as ButtonShadcn } from "../components/ui/buttonShadcn";
 import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog"
+import PreviousMap from "postcss/lib/previous-map";
 
 const TOTAL_DROPS = 16;
 
@@ -36,6 +49,8 @@ export default function Game() {
   const [balance, setBalance] = useState(0);
   const [bettingAmount, setBettingAmount] = useState(0);
   const [multiplier, setMultiplier] = useState(0);
+  const [topUpAmount, setTopUpAmount] = useState(0);
+  const [dialogOpen, setDialogOpen] = useState(false);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
@@ -67,20 +82,6 @@ export default function Game() {
       <div className="flex justify-center items-center" style={{height:"90vh"}}>
         <canvas ref={canvasRef} width="800" height="800"></canvas>
         <div className="flex-col space-y-8">
-
-          <TextField
-            className="w-full"
-            onChange={(e) => setBalance(Number(e.target.value))}
-            value={balance}
-            type="number"
-            label="Balance (Baht)"
-            InputProps={{
-              className: "text-white"
-            }}
-            InputLabelProps={{
-              className: "text-white"
-            }}
-          />
           
           <span>
             <h1 className="pl-3">Balance: {balance}</h1>
@@ -94,18 +95,74 @@ export default function Game() {
           </div>
 
           <div className="flex flex-col space-y-2 pl-2">
-            <Button variant="contained" color="success" onClick={() => {
+            <ButtonMui variant="contained" color="success" onClick={() => {
               const result = calculateGameResult();
               if (ballManager) {
                 ballManager.addBall(result.point);
               }
               setBalance(balance - bettingAmount)
               console.log("Remaining amount after betting: ", balance)
-            }}>Add ball</Button>
+            }}>Drop ball</ButtonMui>
 
-            <Button variant="contained" color="primary">
+            {/* <ButtonMui variant="contained" color="primary">
               Top Up Balance
-            </Button>
+            </ButtonMui> */}
+
+            <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+
+              <DialogTrigger asChild>
+                <ButtonMui variant="contained" color="primary" onClick={() => {
+                  setDialogOpen(true)
+                  console.log("Opening Dialog...")
+                }}>
+                  Top-Up Balance
+                </ButtonMui>
+              </DialogTrigger>
+
+              <DialogContent>
+
+                <DialogHeader>
+                  <DialogTitle>
+                    Are you sure you want to do this?
+                  </DialogTitle>
+                </DialogHeader>
+
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Name
+                    </Label>
+                    <Input
+                      id="name"
+                      defaultValue="I am a loser gambling addict"
+                      className="col-span-3"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="topup-amount" className="text-right">
+                      Amount (THB):
+                    </Label>
+                    <Input
+                      id="topup-amount"
+                      onChange={(e) => setTopUpAmount(Number(e.target.value))}
+                      type="number"
+                      className="col-span-3"
+                    />
+                  </div>
+                </div>
+
+                <DialogFooter>
+                  <ButtonShadcn type="submit" onClick={() => {
+                    setBalance(prevBalance => prevBalance + topUpAmount)
+                    setTopUpAmount(0) // reset topup amount to zero
+                    setDialogOpen(false) // close dialog box
+                  }}>
+                    Save Changes
+                  </ButtonShadcn>
+                </DialogFooter>
+
+              </DialogContent>
+            </Dialog>
           </div>
 
         </div>
